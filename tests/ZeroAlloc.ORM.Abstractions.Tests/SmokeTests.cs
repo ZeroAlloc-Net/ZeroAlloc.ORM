fb3c089254b66b32c2b1b4212fd1961087a7b2a5
Marcel Roozerkans <marcel@roozekrans.nl>
2026-05-30 16:13:54 +0200

    test(abstractions): bootstrap xUnit test project

---
+++ tests/ZeroAlloc.ORM.Abstractions.Tests/SmokeTests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace ZeroAlloc.ORM.Abstractions.Tests;
+
+public class SmokeTests
+{
+    [Fact]
+    public void TestRigWorks()
+    {
+        Assert.True(1 + 1 == 2);
+    }
+}
